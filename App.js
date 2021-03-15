5c4df8f7bf412147a7c989d8d6b09634bcdb6a47
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-15 14:42:20 +0530

    falkjf
    lkdjaslkj

--- App.js
+++ App.js
@@ -84,22 +84,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f8f8f8',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   text: {
     fontSize: 20,
     color: '#101010',
     marginTop: 60,
-    fontWeight: '700',
+    fontWeight: '700'
   },
   listItem: {
     marginTop: 10,
-    padding: 20,
-    alignItems: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    width: '100%',
+    flexDirection: 'row'
   },
-  listItemText: {
+  coverImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 8
+  },
+  metaInfo: {
+    marginLeft: 10
+  },
+  title: {
     fontSize: 18,
-  },
+    width: 200,
+    padding: 10
+  }
 });
