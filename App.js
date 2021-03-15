6b63c0f831976365829773904030f425c6e3915b
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-15 14:23:39 +0530

    lkdlaskjd
    ljalkjdl

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
-import React, {useState,useEffect} from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  ActivityIndicator,
+} from 'react-native';
 
+//target API endpoint - from where we need to pull the data from
+const API_ENDPOINT = `https://randomuser.me/api/?seed=1&page=1&results=20`;
 
 export default function App() {
+  //state variables
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  //call the api API_ENDPOINT
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(API_ENDPOINT)
+      .then(response => response.json())
+      .then(results => {
+        setData(results);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setIsLoading(false);
+        setError(err);
+      });
+  }, []);
+
+  //UI components to get rendered
   return (
     <View style={styles.container}>
       <Text style={styles.text}> Flat list example </Text>
@@ -19,7 +51,7 @@ export default function App() {
   );
 }
 
-//mock data
+/*/mock data
 const data = [
   {id: '1', title: 'first item'},
   {id: '2', title: 'second item'},
@@ -27,28 +59,27 @@ const data = [
   {id: '4', title: 'four item'},
 ];
 
-
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f8f8f8',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   text: {
     fontSize: 20,
     color: '#101010',
     marginTop: 60,
-    fontWeight: '700'
+    fontWeight: '700',
   },
   listItem: {
     marginTop: 10,
     padding: 20,
     alignItems: 'center',
     backgroundColor: '#fff',
-    width: '100%'
+    width: '100%',
   },
   listItemText: {
-    fontSize: 18
-  }
+    fontSize: 18,
+  },
 });
-
